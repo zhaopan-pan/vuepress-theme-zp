fdf5a2c2fe48b2bb7f84fdee28f4013bbd369a33
zhaopan <zhaopanhw@foxmail.com>
2023-06-13 19:09:40 +0800

    feat: pwa添加默认值

--- packages/vuepress-theme-zp/src/node/config/plugin/pwa.ts
+++ packages/vuepress-theme-zp/src/node/config/plugin/pwa.ts
@@ -10,8 +10,10 @@ import {
  * https://v2.vuepress.vuejs.org/zh/reference/plugin/pwa.html
  * @returns Plugin
  */
-export const pwa = (options: PwaPluginOptions, skipWaiting: boolean): Plugin =>
-  pwaPlugin({ ...(options || {}), skipWaiting })
+export const pwa = (
+  options: PwaPluginOptions = {},
+  skipWaiting: boolean
+): Plugin => pwaPlugin({ ...options, skipWaiting })
 
 /**
  * 弹出手动刷新弹窗
@@ -19,5 +21,5 @@ export const pwa = (options: PwaPluginOptions, skipWaiting: boolean): Plugin =>
  * 该插件必须和 pwa 插件 一起使用，并且 skipWaiting 配置项不能设置为 true
  * @returns Plugin
  */
-export const pwaPopup = (options: PwaPopupPluginOptions): Plugin | [] =>
+export const pwaPopup = (options: PwaPopupPluginOptions = {}): Plugin | [] =>
   pwaPopupPlugin(options)
